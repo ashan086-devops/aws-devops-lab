ac5e01bd3d4fdc4ba32f6ec5f986595feea58a70
Ashan Wijekulasuriya <ashan086@gmail.com>
2026-08-08 13:01:14 +0530

    Added MySQL integration with Docker Compose health checks

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
 const express = require("express");
+const mysql = require("mysql2/promise");
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello, AWS devops!');
+async function connectDatabase() {
+
+try {
+
+const db = await mysql.createConnection({
+    host: "mysql",
+    user: "root",
+    password: "password123",
+    database: "devopsdb"
 });
 
-app.listen(3000, () => {
-    console.log('Server started on http://localhost:3000');
-});
+        console.log("✅ Connected to MySQL!");
+
+    return db;
+
+
+}  catch (err) {
+
+        console.log("❌ Database connection failed");
+        console.log(err.message);
+           throw err;
+
+    }
+}
+
+connectDatabase()
+    .then(() => {
+        app.listen(3000, () => {
+            console.log("Server started on http://localhost:3000");
+        });
+    })
+    .catch((err) => {
+        console.error("Application startup failed:", err);
+    });
